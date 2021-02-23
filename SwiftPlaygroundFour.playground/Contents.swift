cbea3f97d15fbb4ae97e2f04ecf1a3ce1602d7e7
rbermudez967 <77324380+rbermudez967@users.noreply.github.com>
2021-02-23 10:06:05 -1000

    I completed Part 1 - 6

---
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//part 4
+/*for number in 1...5
+{
+    print (number)
+}*/
+
+var sum = 0
+for number in 1...5
+{
+    sum += number
+    print (sum)
+}
+
+// part 6 - While Loop
+
+var i = 1
+while i <= 10
+{
+     i = i + 1
+    print(i)
+   
+}
